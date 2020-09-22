6d42d208ed6cb7b28f3c8f82cc7b13cad692210d
georgyaliev <georgyaliev@gmail.com>
2020-09-22 23:16:17 +0300

    Return and rewrite function for creationg book with OpenStruct object

--- books.rb
+++ books.rb
@@ -10,25 +10,26 @@ def make_date(date)
 		end
 end
 
+def make_openstruct(book_array)
+	{
+					link: book_array[0],
+					title: book_array[1], 
+					author: book_array[2], 
+					pages: book_array[3].to_i, 
+					date: make_date(book_array[4]), 
+					rating: book_array[5].to_f, 
+					genres: book_array[6]
+	}
+end
+
 books = CSV.parse(File.read("books.txt"), row_sep: "\n", col_sep: "|")
-           .map do |book|
-				OpenStruct.new(
-					link: book[0],
-					title: book[1], 
-					author: book[2], 
-					pages: book[3].to_i, 
-					date: make_date(book[4]), 
-					rating: book[5].to_f, 
-					genres: book[6]
-				)
-			end
+			.map { |book| OpenStruct.new(make_openstruct(book))}
 
-month = books.sort_by {|book| book.date.month}
-			 .map {|book| (book.date.strftime('%B'))}.uniq
-
-def month_statistics(books, month)
-    month.map do |m|
+def month_statistics(books)
+    books.sort_by {|book| book.date.month}
+		 .map {|book| (book.date.strftime('%B'))}.uniq
+    	 .map do |m|
     		puts "There are #{books.select { |book| book.date.strftime('%B') == m}.size} books were written in #{m} "
     end
 end
-month_statistics(books, month)
+month_statistics(books)
